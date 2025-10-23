9f14ff37acc92d00e10612ce785ce2e378823325
Leandro Punzano <leandropsno@gmail.com>
2025-10-23 14:55:38 +0200

    Implemented typage for PCF Red (Application and Function) and corrected for BOP and IFZ

--- src/main/scala/typer/Typer.scala
+++ src/main/scala/typer/Typer.scala
@@ -1,7 +1,6 @@
 package typer
 
 import parser.*
-import evaluator.*
 import unify.TVar
 
 type TypeEnv = Map[String, Type]
@@ -17,31 +16,33 @@ object Typer {
       val valueType = eval(value, env)
       eval(body, env + (name -> valueType))
 
-    //case Application(func, arg) =>
-      //val funcType = eval(func, env)
-      //funcType
+    case Application(func, arg) =>
+      val funcType = eval(func, env)
+      val argType = eval(arg, env)
+      val resultType = TVar()
+      if (funcType === FUNCTION(argType, resultType)) then resultType
+      else throw new RuntimeException(s"Function has type $funcType, but got argument of type $argType")
 
-    //case Function(param, body) =>
-      //FUNCTION()
-      //eval(body, env + (param -> TVar()))
+    case Function(param, body) =>
+      val paramType = TVar()
+      val bodyType = eval(body, env + (param -> paramType))
+      FUNCTION(paramType, bodyType)
+
+    //case Fix(param, body) =>
 
     case BinaryOperation(left, op, right) =>
       val leftType = eval(left, env)
       val rightType = eval(right, env)
-      (leftType, rightType) match
-        case (INT, INT) => INT
-        case _ => throw new RuntimeException(s"Unexpected type(s) for operator $op: $leftType and $rightType (expected INT and INT)")
+      if (leftType === rightType) then INT
+      else throw new RuntimeException(s"Unexpected type(s) for operator $op: $leftType and $rightType (expected INT and INT)")
 
     case IfZero(cond, thenBranch, elseBranch) =>
       val condType = eval(cond, env)
       val thenType = eval(thenBranch, env)
       val elseType = eval(elseBranch, env)
       condType match
-        case INT => Evaluator.eval(cond, Map()) match {
-          case IntValue(0) => thenType
-          case _ => elseType
-        }
+        case INT =>
+          if (thenType === elseType) then thenType
+          else throw new RuntimeException(s"Types of IFZ branches are different: $thenType and $elseType")
         case _ => throw new RuntimeException(s"Unexpected type in ifz condition: $condType (expected INT)")
-
-    case _ => ???
 }
